45a14f77b60563ce954c4fac4b22295656a07dd2
EricYin <public_2011_2011@qq.com>
2026-09-16 20:42:09 +0800

    Update update-packages.sh

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -135,12 +135,12 @@ echo "Starting package updates..."
 #echo "=========================================="
 #echo "Removing conflicting sing-box packages from feeds..."
 #echo "=========================================="
-#rm -rf ../feeds/packages/net/sing-box
-#rm -rf ../package/feeds/packages/sing-box
+rm -rf ../feeds/packages/net/sing-box
+rm -rf ../package/feeds/packages/sing-box
 #echo "Done removing sing-box from feeds"
 
 # HomeProxy (代理软件) - 使用第5个参数指定额外要删除的包名
-#pkgs=("sing-box"); UPDATE_PACKAGE pkgs "ericyin/luci-app-homeproxy" "main" "pkg"; unset pkgs
+pkgs=("sing-box"); UPDATE_PACKAGE pkgs "ericyin/luci-app-homeproxy" "main" "pkg"; unset pkgs
 #pkgs=("sing-box" "luci-app-homeproxy"); UPDATE_PACKAGE pkgs "ericyin/luci-app-homeproxy" "main" "pkg"; unset pkgs
 pkgs=("homeproxy"); UPDATE_PACKAGE pkgs "immortalwrt/homeproxy" "master"; unset pkgs
 
