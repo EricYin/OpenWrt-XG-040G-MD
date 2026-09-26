408ad5a633d0417c83a655a480b92da25bff38df
EricYin <public_2011_2011@qq.com>
2026-09-26 19:12:55 +0800

    Update InstallAny.sh

--- scripts/InstallAny.sh
+++ scripts/InstallAny.sh
@@ -2,6 +2,6 @@
 mkdir -p ./files/etc/init.d/ 
 mkdir -p ./files/usr/bin
 mkdir -p ./files/etc/uci-defaults
-cp ../../scripts/dynv6.sh ./files/usr/bin/dynv6.sh
-cp ../../scripts/dynv6d ./files/etc/init.d/dynv6d
-cp ../../scripts/99-init-dynv6 ./files/etc/uci-defaults/99-init-dynv6
+cp ../scripts/dynv6.sh ./files/usr/bin/dynv6.sh
+cp ../scripts/dynv6d ./files/etc/init.d/dynv6d
+cp ../scripts/99-init-dynv6 ./files/etc/uci-defaults/99-init-dynv6
