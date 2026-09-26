1cab840188233637d743eef611dad1fa24485b2e
EricYin <public_2011_2011@qq.com>
2026-09-26 19:10:29 +0800

    Create InstallAny.sh

---
+++ scripts/InstallAny.sh
@@ -0,0 +1,7 @@
+#./openwrt
+mkdir -p ./files/etc/init.d/ 
+mkdir -p ./files/usr/bin
+mkdir -p ./files/etc/uci-defaults
+cp ../../scripts/dynv6.sh ./files/usr/bin/dynv6.sh
+cp ../../scripts/dynv6d ./files/etc/init.d/dynv6d
+cp ../../scripts/99-init-dynv6 ./files/etc/uci-defaults/99-init-dynv6
